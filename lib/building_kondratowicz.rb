e1a16ec374930354a018a2efef66983e4c69ad90
EatYourSoup7 <r.kondratowicz1@gmail.com>
2020-02-17 14:49:04 -0600

    initial solution

---
+++ lib/building_kondratowicz.rb
@@ -0,0 +1,52 @@
+require "building_kondratowicz/version"
+
+module BuildingKondratowicz
+  class Error < StandardError; end
+
+  class Building
+    attr_reader :id, :name, :address, :height, :construction_date, :architect
+
+    def initialize(input_options)
+      @id = input_options["id"]
+      @name = input_options["name"]
+      @address = input_options["address"]
+      @height = input_options["height"]
+      @construction_date = input_options["construction_date"]
+      @architect = input_options["architect"]
+    end
+
+    def self.all
+      response = HTTP.get("http://localhost:3000/api/buildings")
+      response.parse.map do |data|
+        Building.new(data)
+      end
+    end
+
+    def self.create(input_options)
+      response = HTTP.post(
+                          "http://localhost:3000/api/buildings", 
+                          form: input_options
+                          )
+      Building.new(response.parse)
+    end
+
+    def self.find(input_id)d
+      response = HTTP.get("http://localhost:3000/api/buildings/#{input_id}")
+      Building.new(response.parse)
+    end
+
+    def update(input_options)
+      response = HTTP.patch(
+                          "http://localhost:3000/api/buildings/#{self.id}", 
+                          form: input_options
+                          )
+      Building.new(response.parse)
+    end
+
+    def destroy
+      response = HTTP.delete("http://localhost:3000/api/buildings/#{self.id}")
+    end
+
+  end
+  
+end
---
+++ lib/building_kondratowicz/version.rb
@@ -0,0 +1,3 @@
+module BuildingKondratowicz
+  VERSION = "0.1.0"
+end
